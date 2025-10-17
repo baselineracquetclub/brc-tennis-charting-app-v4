0ee55ec89590d8fb10d9e18e210e965eb650b5af
baselineracquetclub <zbtprints@gmail.com>
2025-10-16 23:32:16 -0500

    Add files via upload

---
+++ src/components/ui/Button.js
@@ -0,0 +1,23 @@
+
+import React, { useState } from 'react';
+
+export const Button = ({ children, onClick, className = '', active = false }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const pressed = isPressed || active;
+  const cls = `${className} big-btn ${pressed ? 'pressed' : ''}`.trim();
+
+  return (
+    <button
+      onMouseDown={() => setIsPressed(true)}
+      onMouseUp={() => setIsPressed(false)}
+      onMouseLeave={() => setIsPressed(false)}
+      onTouchStart={() => setIsPressed(true)}
+      onTouchEnd={() => setIsPressed(false)}
+      onClick={onClick}
+      className={cls}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+};
